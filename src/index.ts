40d2b5e5ebe47968802b1eb59e7de454b1a9572b
PinBib <105994863+Pinbib@users.noreply.github.com>
2025-05-15 21:19:40 +0100

    the main command handler is done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+import qp from "qp-color";
+import {version} from "../package.json";
+
+try {
+	const args = process.argv.slice(2);
+
+	if (args.length === 0) {
+		throw new Error(qp.yb("You did not specify any command."));
+	} else {
+		switch (args[0]) {
+			case "help":
+				break;
+			case "version":
+				console.log(qp.yi("v" + version));
+				break;
+			case "-v":
+				console.log(qp.yi("v" + version));
+				break;
+			default:
+				throw new Error(qp.rb("Unknown command, make sure it exists use:\n\t") + qp.yi("ima help"));
+		}
+	}
+} catch (err) {
+	console.error((err as Error).message);
+}
