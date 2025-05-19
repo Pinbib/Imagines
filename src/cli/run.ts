778830988103218911c4a86528868d310b52257b
PinBib <105994863+Pinbib@users.noreply.github.com>
2025-05-19 20:34:47 +0100

    made a 'run' command handler

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -1,3 +1,36 @@
+import {resolve} from "path";
+import {existsSync, statSync} from "fs";
+
+import qp from "qp-color";
+import ImageLSB from "../LSB";
+import Runner from "../Runner";
+
 export default function Run(args: string[]): void {
-	
+	if (args[1] !== undefined) {
+		const src: string = resolve(args.slice(1).join(" "));
+
+		if (!existsSync(src)) {
+			throw new Error(qp.rb("The path does not exist.\n\t") + qp.yi("ima run <path>"));
+		}
+
+		if (!statSync(src).isFile()) {
+			throw new Error(qp.rb("The path is not a file.\n\t") + qp.yi("ima run <path>"));
+		}
+
+		ImageLSB.decode(src, (text: string) => {
+			if (text.length > 0) {
+				let matches: RegExpMatchArray | null = text.match(/{([^{}]+)}(.?)+<([^<>]+)>/);
+
+				if (matches !== null) {
+					let [_, com, __, ext]: string[] = matches;
+
+					Runner(com, ext, text);
+				} else {
+					console.log(qp.rb("No code specification found in the image."));
+				}
+			}
+		});
+	} else {
+		throw new Error(qp.rb("You did not specify any path.\n\t") + qp.yi("ima run <path>"));
+	}
 }
